b6e3fb33c6150fe61d55a60d812b4d492910b2ce
PRAKHAR-SRIVASTAVA-Harsh <prkhrsh7@gmail.com>
2022-12-08 20:57:57 +0530

    adding 3 ml models

--- newpythonmodelfile.py
+++ newpythonmodelfile.py
@@ -6,6 +6,15 @@ def sub(a,b):
 
 print ( 'sub is', sub(3,4))
 
+def model1():
+    print('m1')
+
+def model2():
+    print('m2')
+
+    def model3():
+    print('m3')
+
 print('TATA')
 
 print('BYE')
